8a280f1faad922b87ae81ef444d29924b3a1eeb9
Alex Grundwerg <agrundw@gmail.com>
2016-10-01 23:05:58 -0400

    manual merge with ameen

--- Tanks.rkt
+++ Tanks.rkt
@@ -1,4 +1,5 @@
 #lang racket
+
 (require math/matrix)
 (require lang/posn)
 (require math)
@@ -28,8 +29,43 @@
 ;; prop is a function that describes how this bullet acts
 ;; both movement, and special affects
 ;; col? will determine if the bullet has hit anything
-(struct bullet (pos prop col?) #:mutable)
+(struct bullet (pos prop col? on-col) #:mutable)
 
+;; World:
+;; t1, t2 is a tank struct
+;; terr is a Terrain
+;; lob is a Listof Bullets
+;; state is one of:
+;; 'turn1
+;; 'turn2
+;; 'animating
+(struct world (t1 t2 terr lob state) #:mutable)
+
+;; default bullet propagater
+(define (prop-bullet pos pow ang)
+  (let* ([k .4]
+         [vel (make-posn (* k (cos ang) pow) (* k (sin ang) pow))])
+    (λ (bul) 
+      (set-posn-x! pos (+ (posn-x pos) (posn-x vel)))
+      (set-posn-y! pos (+ (posn-y pos) (posn-y vel)))
+      (set-bullet-pos! bul pos)
+      (set-posn-y! vel (- (posn-y vel) .6))
+      (list bul))))
+
+;; default collision checker
+(define (bullet-collide? b world)
+  (and (not (or (< (posn-x (bullet-pos b)) 0)
+                (> (posn-x (bullet-pos b)) WIDTH)))
+       (let* ([terr (world-terr world)]
+              [height (vector-ref terr
+                                  (inexact->exact (round (posn-x (bullet-pos b)))))])
+         (<= (posn-y (bullet-pos b)) height))))
+
+;; default collider
+(define (collide-bullet b world)
+  world)
+
+;; generates terrain
 (define (generate-terrain w h d r)
   (let* ([power (inexact->exact (expt 2 (ceiling (/ (log w) (log 2)))))]
          [ground (make-vector (+ power 1))]
@@ -55,55 +91,70 @@
 
 ;; Terrain is a m x n matrix
 (define terrain (generate-terrain WIDTH HEIGHT 400 .55))
-
-;; World:
-;; t1, t2 is a tank struct
-;; terr is a Terrain
-;; lob is a Listof Bullets
-;; State is one of
-;; - 't1
-;; - 't2
-;; - 'anim
-(struct world (t1 t2 terr lob state) #:mutable)
+;; starting world
 (define init (world tank1 tank2 terrain '() 't1))
 
+;; draws the terrain
 (define (draw-terrain t)
   (for/fold ([scene (empty-scene 1000 600)])
             ([val t]
              [x WIDTH])
     (add-line scene x HEIGHT x val "fuchsia")))
 
+;; render function for big-bang
 (define (render w)
   (let* ([t1 (world-t1 w)]
          [t2 (world-t2 w)]
-         [tn (world-terr w)])
-    (place-images (list (draw-tank t1 tn)
-                        (draw-tank t2 tn))
-                  (list (make-posn (posn-x (tank-pos t1))
-                                   (- (vector-ref tn (posn-x (tank-pos t1))) 8))
-                        (make-posn (posn-x (tank-pos t2))
-                                   (- (vector-ref tn (posn-x (tank-pos t2))) 8)))
-                  (draw-terrain tn))
+         [tn (world-terr w)]
+         [twt (place-images (list (draw-tank t1 tn)
+                                  (draw-tank t2 tn))
+                            (list (make-posn (posn-x (tank-pos t1))
+                                             (- (vector-ref tn (posn-x (tank-pos t1))) 8))
+                                  (make-posn (posn-x (tank-pos t2))
+                                             (- (vector-ref tn (posn-x (tank-pos t2))) 8)))
+                            (draw-terrain tn))])
+    
+    (for/fold ([scene twt])
+              ([val (world-lob w)])
+      (place-image (circle 5 "solid" "red")
+                   (posn-x (bullet-pos val))
+                   (- HEIGHT (posn-y (bullet-pos val)))
+                   scene))
     ))
 
 (define (handle-key w k)
   (let ([cur-tank (if (symbol=? 't1 (world-state w))
                       (world-t1 w)
-                      (world-t2 w))])
-      (cond [(key=? " " k)
-             (set-world-terr! w (generate-terrain WIDTH HEIGHT 400 .55))]
-            [(key=? "d" k) (set-posn-x! (tank-pos cur-tank)
-                                        (+ (posn-x (tank-pos cur-tank)) 1))]
-            [(key=? "a" k) (set-posn-x! (tank-pos cur-tank)
-                                        (- (posn-x (tank-pos cur-tank)) 1))]
-            [(key=? "e" k) (set-tank-ang! cur-tank
-                                          (+ (tank-ang cur-tank)
-                                             (degrees->radians 1)))]
-            [(key=? "q" k) (set-tank-ang! cur-tank
-                                          (- (tank-ang cur-tank)
-                                             (degrees->radians 1)))]))
+                      (world-t2 w))]
+        [tn (world-terr w)])
+    (cond [(key=? "shift" k)
+           (set-world-terr! w (generate-terrain WIDTH HEIGHT 400 .55))]
+          [(or (symbol=? (world-state w) 'anim1) (symbol=? (world-state w) 'anim2)) w]
+          [(key=? "d" k) (set-posn-x! (tank-pos cur-tank)
+                                      (+ (posn-x (tank-pos cur-tank)) 1))]
+          [(key=? "a" k) (set-posn-x! (tank-pos cur-tank)
+                                      (- (posn-x (tank-pos cur-tank)) 1))]
+          [(key=? "e" k) (set-tank-ang! cur-tank
+                                        (+ (tank-ang cur-tank)
+                                           (degrees->radians 1)))]
+          [(key=? "q" k) (set-tank-ang! cur-tank
+                                        (- (tank-ang cur-tank)
+                                           (degrees->radians 1)))]
+          [(key=? " " k)
+           (set-world-lob! w
+                           (list (bullet (make-posn (posn-x (tank-pos cur-tank)) (+ 10 (vector-ref tn (posn-x (tank-pos cur-tank)))))
+                                         (prop-bullet (make-posn (posn-x (tank-pos cur-tank)) (+ 10 (vector-ref tn (posn-x (tank-pos cur-tank)))))
+                                                      50
+                                                      (tank-ang cur-tank))
+                                         bullet-collide?
+                                         collide-bullet)))
+           (if (symbol=? 't1 (world-state w))
+               (set-world-state! w 'anim1)
+               (set-world-state! w 'anim2))]))
   w)
 
+
+;; draws a tank
 (define (draw-tank tk tn)
   (let* ([x (posn-x (tank-pos tk))]
          [y (vector-ref tn x)]
@@ -121,6 +172,7 @@
                       (pen "black" 3 "solid" "butt" "bevel")))
     ))
 
+;; tank image
 (define (tank-image c)
   (polygon (list (make-posn 0 0)
                  (make-posn 5 0)
@@ -133,9 +185,25 @@
            "solid"
            c))
 
+(define (tick w)
+  (cond [(or (symbol=? (world-state w) 'anim1)
+             (symbol=? (world-state w) 'anim2))
+         (let* ([lob '()])
+           (for ([bul (world-lob w)])
+                (if ((bullet-col? bul) bul w)
+                    ((bullet-on-col bul) bul w)
+                    (set! lob (append ((bullet-prop bul) bul) lob))))
+           (set-world-lob! w lob)
+           (when (empty? lob)
+             (set-world-state! w (if (symbol=? (world-state w) 'anim1)
+                                     't2
+                                     't1))))
+         w]
+        [else w]))
+
 (define (play init)
   (big-bang init
-            ;(on-tick ...)
+            (on-tick tick)
             (to-draw render)
             ;(on-mouse ...)
             (on-key handle-key)
